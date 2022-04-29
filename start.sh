cb316dde176d799565858f878ab9369ef1379591
jmheqiao <78342744+jmheqiao@users.noreply.github.com>
2022-04-29 13:56:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 echo "REFRESH_TOKEN: $REFRESH_TOKEN"
 echo "PORT: $PORT"
-/aliyundrive-webdav --refresh-token $REFRESH_TOKEN --port $PORT --auto-index
+/usr/bin/aliyundrive-webdav --refresh-token $REFRESH_TOKEN --port $PORT --auto-index
